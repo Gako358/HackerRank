f0815926c4233fff1faf145e7163347e071aa037
Knut Øien <gako358@outlook.com>
2020-10-08 21:26:22 +0200

    completed time converse

---
+++ timeConversion/timeConverse.c
@@ -0,0 +1,48 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+
+/*
+ * Complete the timeConversion function below.
+ * If time is 0350 convert to 1550
+ * if time is 1550 conver to 0350
+ */
+
+/*
+ * Please either make the string static or allocate on the heap. For example,
+ * static char str[] = "hello world";
+ * return str;
+ *
+ * OR
+ *
+ * char* str = "hello world";
+ * return str;
+ *
+ */
+char* timeConversion(char* s) {
+    /*
+     * Write your code here.
+     */
+
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char* s = readline();
+
+    char* result = timeConversion(s);
+
+    fprintf(fptr, "%s\n", result);
+
+    fclose(fptr);
+
+    return 0;
+}
